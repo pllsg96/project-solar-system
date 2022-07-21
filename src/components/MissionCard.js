d3f01c915a49b8b80a8807571b0763b9b88795aa
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-21 19:34:04 -0300

    Adiciona requisito 9

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 
 class MissionCard extends Component {
   render() {
-    return <div data-testid="mission-card" />;
+    const { name, year, country, destination } = this.props;
+    return (
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{name}</p>
+        <p data-testid="mission-year">{year}</p>
+        <p data-testid="mission-country">{country}</p>
+        <p data-testid="mission-destination">{destination}</p>
+      </div>
+    );
   }
 }
+
+
 
 export default MissionCard;
